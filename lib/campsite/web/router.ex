8b73d758011d8e9b9422609d2b965835132275f0
divya <hello@shortdiv.com>
2024-08-09 21:32:46 -0400

    adding more controller logic

--- lib/campsite/web/router.ex
+++ lib/campsite/web/router.ex
@@ -1,17 +1,23 @@
 defmodule Campsite.Web.Router do
-  def call(conn) do
-    content_for(conn.req_path, conn)
-  end
+  use Sparrow.Router
+  # import Plugs.Conn
 
-  defp content_for("/", conn) do
-    conn |> Campsite.Web.PageController.call(:home)
-  end
+  alias Campsite.Web.PageController
 
-  defp content_for("/2", conn) do
-    conn |> Campsite.Web.PageController.call(:two)
-  end
+  get "/", PageController, :home
+  # def call(conn) do
+  #   content_for(conn.req_path, conn)
+  # end
 
-  defp content_for(_, conn) do
-    conn |> Campsite.Web.PageController.call(:not_matched)
-  end
+  # defp content_for("/", conn) do
+  #   conn |> Campsite.Web.PageController.call(:home)
+  # end
+
+  # defp content_for("/2", conn) do
+  #   conn |> Campsite.Web.PageController.call(:two)
+  # end
+
+  # defp content_for(_, conn) do
+  #   conn |> Campsite.Web.PageController.call(:not_matched)
+  # end
 end
