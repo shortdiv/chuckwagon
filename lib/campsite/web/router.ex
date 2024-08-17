c723d6f5c8004f3e2f239acaf9000116b885bb46
divya <hello@shortdiv.com>
2024-08-16 21:31:28 -0400

    fix: make plugs work with custom router

--- lib/campsite/web/router.ex
+++ lib/campsite/web/router.ex
@@ -1,6 +1,8 @@
 defmodule Campsite.Web.Router do
   use Spaghetti.Router
 
+  plug Plugs.Logger, log: :info
+
   get "/hello" do
     send_resp(conn, 200, "world")
   end
--- lib/spaghetti/router.ex
+++ lib/spaghetti/router.ex
@@ -3,10 +3,15 @@ defmodule Spaghetti.Router do
     quote do
       import Spaghetti.Router
       import Plug.Conn
+      import Plug
+
+      @before_compile Spaghetti.Router
+      @plugs []
 
       def init(opts), do: opts
 
       def call(conn, _opts) do
+        conn = apply_plugs(conn)
         content_for(conn.request_path, conn)
       end
     end
@@ -24,6 +29,22 @@ defmodule Spaghetti.Router do
     quote do
       defp content_for(_, var!(conn)) do
         unquote(block)
+      end
+    end
+  end
+
+  defmacro plug(plug_name, opts) do
+    quote do
+      @plugs [{unquote(plug_name), unquote(opts)} | @plugs]
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      defp apply_plugs(conn) do
+        Enum.reduce(@plugs, conn, fn {plug, opts}, acc ->
+          apply(plug, :call, [acc, opts])
+        end)
       end
     end
   end
